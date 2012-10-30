9464e7c27a01fbd4f1912266816f624129017f2c
dbc-apprentice <apprentice@devbootcamp.com>
2012-10-29 19:31:15 -0700

    targeting href with nokogiri

--- search_results.rb
+++ search_results.rb
@@ -1,9 +1,49 @@
+require 'nokogiri'
+require 'open-uri'
+require 'fakeweb'
+
 class SearchResult
+attr_reader :url, :search_date, :search_term
 	#url, date, terms
 	#methods
 		#parse
 		#save
+	def initialize(url, search_term)
+		@url = url
+		@search_date = Time.now
+		@search_term = search_term
+		apartment_result = File.read("craigslist_test.html")
+		FakeWeb.register_uri(:get, "http://sfbay.craigslist.org/pen/apa/3372916109.html", :body => apartment_result)
+		@doc = Nokogiri::HTML(open('http://sfbay.craigslist.org/pen/apa/3372916109.html'))
+
+	end
+
+	def parse
+		#puts @doc.inspect
+		@doc.css("p [class = 'row']/a").each do |link|
+			puts link['href']
+
+		#@doc.xpath('//p[class="row"]').each do |link|
+			
+			#puts link[:href]
+		end
+		# url_array = []
+		# doc = Nokogiri::HTML(open(@url))
+		# doc.css("p [class = 'row']").each do |link|
+		# 	p link.content
+		# end
+	end
 
 
+
+			
+		# 	puts link
+		# 	url_array << link
+		# end
+		# url_array
+	
 
 end
+
+test = SearchResult.new('http://sfbay.craigslist.org/pen/apa/3372916109.html', 'bike')
+test.parse
--- search_spec.rb
+++ search_spec.rb
@@ -1,10 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require_relative 'search_results'
-require 'rspec'
-require './search_data.rb'
-require 'nokogiri'
-require 'open-uri'
+#require 'fakeweb'
 
 class Posting
 	attr_accessor :url
@@ -15,19 +12,58 @@ end
 
 
 describe SearchResult do
-	let (:result) {SearchResult.new}
+	before(:each) do
+		apartment_result = File.read("craigslist_test.html")
+		FakeWeb.register_uri(:get, "http://sfbay.craigslist.org/pen/apa/3372916109.html", :body => apartment_result)
+		@doc = Nokogiri::HTML(open('http://sfbay.craigslist.org/pen/apa/3372916109.html'))
+	end
+
+	let (:result) {SearchResult.new('www.google.com', 'bike')}
 
 	context "#initialize" do 
 		it "returns a search result" do 
 			result.should be_an_instance_of SearchResult
 		end
 
-		it "contains the search result url"
+		it "responds to url" do
+			result.should respond_to(:url)
+		end
 
-		it "contains the search date"
+		it "responds to search_date" do
+			result.should respond_to(:search_date)
+		end
 
-		it "contains the search term"
+		it "responds to search term" do
+			result.should respond_to(:search_term)
+		end
 
+		it "returns a url" do 
+			result.url.should == 'www.google.com'
+		end
+
+		it "search date is equal to timestamp" do 
+			result.search_date.should be <= Time.now
+		end
+
+		it "returns the search term" do
+			result.search_term.should eq "bike"
+		end
+
+	end
+
+
+
+
+	context "#parse" do
+		let (:result) {SearchResult.new("http://sfbay.craigslist.org/pen/apa/3372916109.html", 'bike')}
+		#mock = result.stub!(:parse).and_return
+		it "returns a collection of urls" do
+			p urls = result.parse
+			urls.each do |url|
+				#puts url.inspect
+				#url.should_not match(/(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/)
+			end
+		end
 	end
 
 	
