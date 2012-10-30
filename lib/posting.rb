52eecd26d62a7870708a67fe4dcbc3098161d594
Nima Mehanian <nima.mehanian@gmail.com>
2012-10-29 21:55:38 -0700

    added save method,and price and date parsing methods

--- lib/posting.rb
+++ lib/posting.rb
@@ -1,7 +1,3 @@
-# Posting.new(url)
-# .parse
-
-
 
 class Posting
 
@@ -9,15 +5,39 @@ class Posting
     @doc = Nokogiri::HTML(open(url))
     @url = url
     @title = title
-    #   @location
-
+    @location = location
+    @date = date
+    @price = price
   end
 
   def title
-    @title = @doc.css('h2')[0].content
+    #@title = @doc.css('h2')[0].content
+    @title = @doc.css('title')[0].content
   end
 
   def location
+    from_webpage = @doc.xpath('//div[@class="bchead"]/a')[1].content
+    @location = from_webpage.split[0..-2].join(" ")
   end
 
+  def date
+    from_webpage = @doc.xpath('//span[@class="postingdate"]')[0].content
+    @date = from_webpage.split[1..-1].join(" ")
+  end
+
+  def price
+    @price = @doc.css('h2')[0].content.gsub(/(.*)(\$)(\d+)(.*)/,'\3')
+  end
+
+  def add_db(database)
+    @database = database
+  end
+
+  def save
+    insert_info = <<-eos
+    INSERT INTO postings(title, url, price, location)
+    VALUES("#{@title}", "#{@url}", #{@price.to_i}, "#{@location}");
+    eos
+    @database.execute(insert_info)
+  end
 end
--- spec/posting_spec.rb
+++ spec/posting_spec.rb
@@ -1,6 +1,7 @@
 require 'fakeweb'
 require 'nokogiri'
 require 'open-uri'
+require "sqlite3"
 require "rspec"
 require "./lib/posting.rb"
 
@@ -8,46 +9,61 @@ require "./lib/posting.rb"
 
 
 describe Posting do
+  let(:posting) { Posting.new("http://sfbay.craigslist.org/pen/apa/3372916109.html") }
   before :each do
     apartment_result = File.read("spec/fixtures/craigslist_sample_listing.html")
     FakeWeb.register_uri(:get, "http://sfbay.craigslist.org/pen/apa/3372916109.html", :body => apartment_result)
-    @doc = Nokogiri::HTML(open('http://sfbay.craigslist.org/pen/apa/3372916109.html'))
-    # posting = Posting.new("http://example.com/test1")
-    #    before(:each) do
-    #        Interface.any_instance.stub(:gets).and_return("john", "johnspassword")
-    #        create_interface(sf_gen)
-    #      end
-    #
+    #@doc = Nokogiri::HTML(open('http://sfbay.craigslist.org/pen/apa/3372916109.html'))
   end
 
   context ".initialize" do
-  #Posting.new
-  #Posting.any_instance("http://sfbay.craigslist.org/pen/apa/3372916109.html")
-    before :each do
-      @posting = Posting.new("http://sfbay.craigslist.org/pen/apa/3372916109.html")
-    end
+    # before :each do
+    #       @posting = Posting.new("http://sfbay.craigslist.org/pen/apa/3372916109.html")
+    #     end
     it "initializes with title of posting" do
-      @posting.title.should include("THANKSGIVING IN TAHOE -- Wonderful house/location, hot tub, sleeps 10+")
+      posting.title.should include("THANKSGIVING IN TAHOE -- Wonderful house/location, hot tub, sleeps 10+")
     end
 
-
-    # it "initializes the date posted" do
-    #      @posting.date_posted.should eq("2012-10-29, 11:19AM PDT")
-    #    end
-    #
     it "initializes the location" do
-      @posting.location.should eq("SF bay area")
+      posting.location.should eq("SF bay area")
     end
-    #
-    #    it "initializes the price" do
-    #      @posting.price.should eq("SF bay area")
-    #    end
+
+    it "initializes the date posted" do
+      posting.date.should eq("2012-10-29, 11:19AM PDT")
+    end
+
+    it "initializes the price" do
+     posting.price.should eq("300")
+    end
+
+    #it "has a category"
+
   end
 
+   context "#save" do
+     before :each do
+       @database = SQLite3::Database.new("test.db")
+       @database.execute("DROP TABLE IF EXISTS postings;")
+       create_table = <<-eos
+       CREATE TABLE postings (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title VARCHAR NOT NULL,
+            url VARCHAR NOT NULL,
+            price INTEGER,
+            location TEXT NOT NULL);
+            eos
+       @database.execute(create_table)
+     end
 
-  #  context "#save" do
-  #    it "saves the posting and its attributes to the database"
-  #  end
+     it "saves the posting and its attributes to the database" do
+
+       posting.add_db(@database)
+       posting.save
+
+       puts @database.execute("SELECT * FROM postings").inspect
+       @database.execute("SELECT * FROM postings").length.should eq(1)
+
+     end
+   end
   #
   #  context "#load" do
   #    it "loads the posting from the database"
