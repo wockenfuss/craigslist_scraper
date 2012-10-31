5a60d34b5d294027f26ef6d8d2da383b66bc4f4f
Brent Coughenour + Ami Kehaty <apprentice+bc+ak@devbootcamp.com>
2012-10-30 17:06:08 -0700

    finished #add_row

--- lib/cldatabase.rb
+++ lib/cldatabase.rb
@@ -8,15 +8,39 @@ class CLDatabase
   end
 
   def add_row(table_name, params)
-puts "here"
-    puts field_names = params.keys.join(', ')
-    puts value_names = params.values.join(', ')
+    return false if @database.execute("SELECT url FROM #{table_name}").any? { |url|  url[0] == params[:url][1..-2] }
+    field_names = params.keys.join(', ')
+    value_names = params.values.join(', ')
     insert_info = <<-eos
-     INSERT INTO #{table_name}(#{field_names})
-     VALUES(#{value_names});
-     eos
-     @database.execute(insert_info)
+    INSERT INTO #{table_name}(#{field_names})
+    VALUES(#{value_names});
+    eos
+    @database.execute(insert_info)
+    @database.last_insert_row_id
   end
+
+  def create_tables
+    create_table = <<-eos
+     CREATE TABLE search_results (id INTEGER PRIMARY KEY AUTOINCREMENT,
+          search_term VARCHAR NOT NULL,
+          url VARCHAR NOT NULL,
+          search_date TEXT NOT NULL);
+          eos
+     @database.execute(create_table)
+
+     create_table = <<-eos
+     CREATE TABLE postings (id INTEGER PRIMARY KEY AUTOINCREMENT,
+         title VARCHAR NOT NULL,
+         url VARCHAR NOT NULL,
+         price INTEGER,
+         location TEXT NOT NULL,
+         category TEXT NOT NULL,
+         track_search_result INTEGER,
+         FOREIGN KEY (track_search_result) REFERENCES search_results(id));
+         eos
+     @database.execute(create_table)
+  end
+
 
 
 
--- spec/cldatabase_spec.rb
+++ spec/cldatabase_spec.rb
@@ -10,7 +10,7 @@ describe CLDatabase do
      create_table = <<-eos
      CREATE TABLE search_results (id INTEGER PRIMARY KEY AUTOINCREMENT,
           search_term VARCHAR NOT NULL,
-          search_url VARCHAR NOT NULL,
+          url VARCHAR NOT NULL,
           search_date TEXT NOT NULL);
           eos
      @database.execute(create_table)
@@ -42,42 +42,74 @@ describe CLDatabase do
 
 
     context '#initialize' do
-
+      before(:each) { @cldata2 = CLDatabase.new('testdata.db') }
+      after(:each) { File.delete('testdata.db') }
 
       it 'connects to an existing DB' do
         @cldata.database.execute("SELECT * FROM postings").length.should eq(1)
       end
 
       it "creates a new database if none exists" do
-        @cldata2 = CLDatabase.new('testdata.db')
-
         @cldata2.database.execute("DROP TABLE IF EXISTS search_results;")
-         create_table = <<-eos
-         CREATE TABLE search_results (id INTEGER PRIMARY KEY AUTOINCREMENT,
-              search_term VARCHAR NOT NULL,
-              search_url VARCHAR NOT NULL,
-              search_date TEXT NOT NULL);
-              eos
-         @cldata2.database.execute(create_table)
+        create_table = <<-eos
+        CREATE TABLE search_results (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            search_term VARCHAR NOT NULL,
+            url VARCHAR NOT NULL,
+            search_date TEXT NOT NULL);
+            eos
+        @cldata2.database.execute(create_table)
 
         @cldata2.database.execute("SELECT * FROM search_results").should eq([])
-        File.delete('testdata.db')
+
       end
 
-      it "creates a search_results table the first time it's run"
+      it "creates search_results and postings tables the first time it's run" do
+        @cldata2.create_tables
+        @cldata2.database.execute("SELECT * FROM search_results").should eq([])
+        @cldata2.database.execute("SELECT * FROM postings").should eq([])
+      end
 
       it "creates a postings table the first time it's run"
 
     end
 
     context "#add_row" do
-      it "adds a row to the appropriate table" do
+
+      it "returns an error if the row already exists in the postings database" do
+        @cldata.add_row("postings", { title: '"Bike for sale"', url: '"www.google.com"', price: 150,
+                                                location: '"San Jose"', category: '"Bikes"', track_search_result: 1 })
+        @cldata.add_row("postings", { title: '"Bike for sale"', url: '"www.google.com"', price: 150,
+                                                location: '"San Jose"', category: '"Bikes"' })
+        @cldata.database.execute("SELECT * FROM postings").length.should eq(1)
+      end
+
+      it "returns an error if the row already exists in the search_results database" do
+        @cldata.add_row("search_results", { search_term: '"bike"', url: '"http://www.google.com"', search_date: '"#{Time.now }"'})
+        @cldata.add_row("search_results", { search_term: '"bike"', url: '"http://www.google.com"', search_date: '"#{Time.now }"'})
+        @cldata.database.execute("SELECT * FROM search_results").length.should eq(1)
+      end
+
+      it "adds a row to the postings table" do
         @cldata.add_row("postings", { title: '"Bike for sale"', url: '"www.google.com"', price: 150,
                                               location: '"San Jose"', category: '"Bikes"' })
         @cldata.database.execute("SELECT * FROM postings")[0][1].should eq("Bike for sale")
       end
 
+      it "adds a row to the search_results table" do
+        @cldata.add_row("search_results", { search_term: '"bike"', url: '"http://www.google.com"', search_date: '"#{Time.now }"'})
+        @cldata.database.execute("SELECT * FROM search_results")[0][1].should eq("bike")
+      end
+
+      it "returns the row's primary key after adding the row" do
+        @cldata.database.execute("DELETE FROM search_results;")
+        temp = @cldata.add_row("search_results", { search_term: '"bike"', url: '"http://www.google.com"', search_date: '"#{Time.now }"'})
+        @cldata.database.execute("SELECT id FROM search_results")[0][0].should eq(temp)
+      end
+
     end
+
+    context "#read_row" do
+      it "creates a Posting object with "
 
 
 
